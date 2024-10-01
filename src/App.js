d14ed73fc0fce55c1f38b138afd983d16db558c4
Diego-dev009 <diegomamaniquispe009@gmail.com>
2024-09-30 23:40:36 -0400

    creacionde las gh y git commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import yo from './assets/img/yo.jpg'
+function Curriculun(){
+  return (
+    <div className="curriculum-container">
+    <div className="header">
+      <h1>Diego Mamani Quispe</h1>
+      <h2>Desarrollador de Software</h2>
+    </div>
+    
+    <div className="contact-info">
+      <p><strong>Email:</strong> diegomamaniqusipe009@gmail.com</p>
+      <p><strong>Teléfono:</strong> 73762336</p>
+      <p><strong>Nivel de Inglés:</strong> B2</p>
+    </div>
+    
+    <div className="hobbies">
+      <h3>Hobbies</h3>
+      <ul>
+        <li>Deportes</li>
+        <li>Videojuegos</li>
+        <li>Series</li>
+      </ul>
+    </div>
+  </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className= "App-header">
+        <div class = "contenedor">
+        <img src= {yo} class = "imagen" />
+        </div>
+        <Curriculun/>
       </header>
     </div>
   );
